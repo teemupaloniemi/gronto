0efd476c40d9fb77ec5d711a1700d1a21fd47864
tealjapa <tealjapa@student.jyu.fi>
2025-05-08 11:52:10 +0300

    refactor: utils

---
+++ src/utils.rkt
@@ -0,0 +1,26 @@
+#lang racket
+(require "io.rkt")
+
+;; Utils, I know you hate these.
+;; Thats why they are here!
+(provide range)
+(define (range n)
+  (define (fill l n)
+    (if (= n 0)
+      l
+      (fill (cons n l) (- n 1))))
+  (fill '() n))
+
+;; Get value of a filed corresponding the key in json elm.
+(provide value)
+(define (value key elm)
+  (hash-ref elm key))
+
+;; Given a list of json data search return those that match
+;; the given code in a field "code".
+(provide search-by-code)
+(define (search-by-code data code)
+  (car
+    (filter
+      (lambda (x) (string=? (value 'code x) code))
+      data)))
