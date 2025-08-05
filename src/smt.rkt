d6bd34513cca2a4782d84d6ddc20bd4077295c37
tealjapa <tealjapa@student.jyu.fi>
2025-08-05 21:46:26 +0300

    revert+breaking: Unintentional variables are declare under the hood in rosette.

--- src/smt.rkt
+++ src/smt.rkt
@@ -7,70 +7,56 @@
 (require "utils.rkt")
 
 ;; For solvers.
-(require rosette)
+(require rosette/safe)
 (require rosette/solver/smt/z3)
 
-(define (build-model solver
-                     courses
-                     sem-min
-                     sem-max
-                     min-cred-sem
-                     max-cred-sem)
+(define (build-model solver courses sem-min sem-max min-cred-sem max-cred-sem)
   (let ((sems (for/list ((x (in-range sem-min sem-max))) x))
         ;; These are the SMT symbolic constants.
         (vars (for/list ((c courses))
-                (cons c
-                  (constant (course-code c)
-                            integer?)))))
+                (cons c (constant (course-code c)
+                                  integer?)))))
 
     (for ((v1 vars))
        ;; Assert semester limits to each course variable.
-       (define/contract bs
+       (define/contract bs1
                        (listof boolean?)
                        (list (and (>= (cdr v1) sem-min)
                                   (<= (cdr v1) sem-max))))
-       (solver-assert solver bs)
 
        ;; Assert that prequisites come later that the course (if they exist).
        (for ((v2 vars))
            ;; When c2 is a member of c1 prerequisites
-           (when (member (course-code (car v2))
-                         (course-prerequisite-courses (car v1)))
+           (when (member (course-code (car v1))
+                         (course-prerequisite-courses (car v2)))
              ;; c2 has to be taken before c1.
-             (define/contract bs
+             (define/contract bs2
                               (listof boolean?)
-                              (list (> (cdr v1) (cdr v2))))
-             (solver-assert solver bs))))
+                              (list (> (cdr v2) (cdr v1))))
+             (solver-assert solver bs2)))
+       (solver-assert solver bs1))
+
     ;; Check that each semester has at most/least n credits bound to it.
     (for ((s sems))
       (define/contract bs
                        (listof boolean?)
-                       (list (> (apply + (for/list ((v vars))
-                                                    (if (= (cdr v) s)
-                                                        (course-credits (car v))
-                                                        0)))
-                                min-cred-sem)))
-      (solver-assert solver bs))
-
-    (for ((s sems))
-      (define/contract bs
-                       (listof boolean?)
-                       (list (< (apply + (for/list ((v vars))
-                                                    (if (= (cdr v) s)
-                                                        (course-credits (car v))
-                                                        0)))
-                                max-cred-sem)))
+                       (list (and (>= (apply + (for/list ((v vars))
+                                                         (if (= (cdr v) s)
+                                                             (course-credits (car v))
+                                                             0)))
+                                      min-cred-sem)
+                                  (<= (apply + (for/list ((v vars))
+                                                         (if (= (cdr v) s)
+                                                             (course-credits (car v))
+                                                             0)))
+                                      max-cred-sem))))
       (solver-assert solver bs))
 
     solver))
 
-(define (build-and-solve courses
-                         years
-                         sem-per-year
-                         min-tot-cred
-                         max-tot-cred
-                         min-cred-sem
-                         max-cred-sem)
+(define (build-and-solve courses years sem-per-year min-tot-cred max-tot-cred
+                         min-cred-sem max-cred-sem)
+    (output-smt #t)
     (define solver (z3 'QF_LIA))
     (define mod (build-model solver
                              courses
@@ -79,16 +65,18 @@
                              min-cred-sem
                              max-cred-sem))
     (define result (solver-check mod))
-    (displayln (model result))
-    (sat? result))
+    (hash->list (model result)))
 
 (define (main)
-    (build-and-solve (hash-to-struct (json-read "tmp/output.json"))
-                     3     ;; Years
-                     4     ;; sem-per-year
-                     180   ;; min-tot-cred
-                     10000 ;; max-tot-cred
-                     0     ;; min-sem-cred
-                     20))  ;; max-sem-cred
+    (define result
+      (build-and-solve (hash-to-struct (json-read "tmp/output.json"))
+                       3     ;; Years
+                       4     ;; sem-per-year
+                       180   ;; min-tot-cred
+                       10000 ;; max-tot-cred
+                       0     ;; min-sem-cred
+                       20))  ;; max-sem-cred
+    (for ((p result))
+         (displayln p)))
 
 (main)
