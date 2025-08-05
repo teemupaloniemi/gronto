3f5a3fe8e903953d8a54ea55c8fa3ef01088e38a
tealjapa <tealjapa@student.jyu.fi>
2025-08-05 18:44:11 +0300

    refactor: Split model declaration into small pieces.

--- src/smt.rkt
+++ src/smt.rkt
@@ -10,24 +10,15 @@
 (require rosette)
 (require rosette/solver/smt/z3)
 
-(define (declare solver courses sem-min sem-max min-cred-sem max-cred-sem)
-  (let ((vars (for/list ((c courses))
-                (cons c
-                      (constant (datum->syntax #'courses
-                                               (string->symbol (course-code c)))
-                                integer?))))
-        (sems (for/list ((x (in-range sem-min sem-max)))
-                x)))
-
-    ;; Assert semester limits to each course variable.
+(define (type-limits solver vars sem-min sem-max)
     (for ((v vars))
       (define/contract bs
                        (listof boolean?)
                        (list (and (>= (cdr v) sem-min)
                                   (<= (cdr v) sem-max))))
-      (solver-assert solver bs))
+      (solver-assert solver bs)))
 
-    ;; Assert that prequisites come later that the course (if they exist).
+(define (prerequisites solver vars)
     (for ([v1 vars])
        (for ([v2 vars])
            ;; When c2 is a member of c1 prerequisites
@@ -37,48 +28,64 @@
              (define/contract bs
                               (listof boolean?)
                               (list (> (cdr v1) (cdr v2))))
-             (solver-assert solver bs))))
+             (solver-assert solver bs)))))
 
-    ;; Check that each semester has at most n credits bound to it.
+(define (semester-limits comparator solver vars sems limit)
     (for ((s sems))
       (define/contract bs
                        (listof boolean?)
-                       (list (> (apply + (for/list ((v vars))
-                                           (constant (if (= (cdr v) s)
-                                                     (course-credits (car v))
-                                                     0)
-                                               integer?)))
-                                min-cred-sem)))
-      (solver-assert solver bs))
+                       (list (comparator (apply + (for/list ((v vars))
+                                                    (constant (if (= (cdr v) s)
+                                                                  (course-credits (car v))
+                                                                  0)
+                                                              integer?)))
+                                         limit)))
+      (solver-assert solver bs)))
 
-    (for ((s sems))
-      (define/contract bs
-                       (listof boolean?)
-                       (list (> (apply + (for/list ((v vars))
-                                           (constant (if (= (cdr v) s)
-                                                     (course-credits (car v))
-                                                     0)
-                                               integer?)))
-                                min-cred-sem)))
-      (solver-assert solver bs))
+(define (build-model solver
+                     courses
+                     sem-min
+                     sem-max
+                     min-cred-sem
+                     max-cred-sem)
+  (let ((sems (for/list ((x (in-range sem-min sem-max))) x))
+        ;; These are the SMT symbolic constants.
+        (vars (for/list ((c courses))
+                (cons c
+                  (constant (datum->syntax #'courses
+                                           (string->symbol (course-code c)))
+                            integer?)))))
 
+    ;; Assert semester limits to each course variable.
+    (type-limits solver vars sem-min sem-max)
+
+    ;; Assert that prequisites come later that the course (if they exist).
+    (prerequisites solver vars)
+
+    ;; Check that each semester has at most/least n credits bound to it.
+    (semester-limits >= solver vars sems min-cred-sem)
+    (semester-limits <= solver vars sems max-cred-sem)
     solver))
 
-(define (build-smt-model courses-path
+(define (build-and-solve courses
                          years
                          sem-per-year
                          min-tot-cred
                          max-tot-cred
                          min-cred-sem
                          max-cred-sem)
-  (let ((semesters (* years sem-per-year))
-        (courses   (hash-to-struct (json-read courses-path)))
-        (solver    (z3 'QF_LIA)))
-    (define model (solver-check (declare solver courses 1 semesters min-cred-sem max-cred-sem)))
-    (cons model (sat? model))))
+    (define solver (z3 'QF_LIA))
+    (define model (build-model solver
+                               courses
+                               1
+                               (* years sem-per-year)
+                               min-cred-sem
+                               max-cred-sem))
+    (define result (solver-check model))
+    (cons result (sat? result)))
 
 (define (main)
-  (build-smt-model "tmp/output.json"
+  (build-and-solve (hash-to-struct (json-read "tmp/output.json"))
                    3     ;; Years
                    4     ;; sem-per-year
                    180   ;; min-tot-cred
