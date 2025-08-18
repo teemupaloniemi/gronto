c104100b1e3e75f65deefd94d720b72dd98c3d08
tealjapa <tealjapa@student.jyu.fi>
2025-08-18 17:27:31 +0300

    fix: Generating precomputed needs a placeholder.

---
+++ src/precomputed.rkt
@@ -0,0 +1,6 @@
+#lang racket
+
+;; This is a dummy placeholder for the precomputed distances.
+;; Should be only in this form in version control, not as the precomputed hash.
+(provide precomputed)
+(define precomputed #f)
