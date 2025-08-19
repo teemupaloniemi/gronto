1cc63cb76637299679eab80c4d8b63538bcdc291
Teemu Paloniemi <tealjapa@student.jyu.fi>
2025-08-19 18:02:17 +0300

    refactor: Remove extra utility function.

--- src/utils.rkt
+++ src/utils.rkt
@@ -18,26 +18,6 @@
             (hash-ref c 'skill-prerequisites)
             (hash-ref c 'outcomes))))
 
-
-;; Racket Generic Graph Library utilizes adjacency lists.
-(provide hash-to-adjacency-lists)
-(define (hash-to-adjacency-lists h)
-  (let ((al '()))
-    (define (recurse h prev prevprev)
-      (if (hash? h)
-         (if (eq? prevprev 'None)
-           (set! al (cons (cons prev (hash-keys h)) al))
-           (set! al (cons (cons prev (cons prevprev (hash-keys h))) al)))
-         (set! al (cons (cons prev (list prevprev)) al)))
-      (when (hash? h)
-        (map (lambda (next)
-                    (recurse (hash-ref h next)
-                              next
-                              prev))
-            (hash-keys h))))
-    (recurse h 'Root 'None)
-    al))
-
 ;; Given a list of courses search those that match the given code.
 (provide search-by-code)
 (define (search-by-code courses code type)
