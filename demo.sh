8998b77e3821918587bb422ca08df251198fbe5a
Teemu Paloniemi <tealjapa@student.jyu.fi>
2026-07-07 00:23:05 +0300

    Simpler demo.

--- demo.sh
+++ demo.sh
@@ -3,21 +3,21 @@ make
 
 # Compute prerequisites
 # Params:
-#   [in]  "data/input.json"       individual courses in questionnaire format
-#   [out] "tmp/prerequisites.dot" result dot graph
+#   [in]  "data/input.json"       courses path
+#   [out] "tmp/prerequisites.dot" result dot graph path
 #   [out] "tmp/output.json"       amended version of input courses
 #   [in]  "30"                    threshold for prerequisiteness (experimental)
 racket ./src/prerequisites.rkt data/input.json tmp/prerequisites.dot tmp/output.json 30
 
 # Schedule the courses (if possible*)
 # Params:
-#   [in]  "tmp/output.json"       individual courses in questionnaire format
-#   [out] "tmp/schedule.dot"      result dot graph
-#   [in]  "2"                     years in the curriculum
+#   [in]  "tmp/output.json"       courses path
+#   [out] "tmp/schedule.dot"      result dot graph path
+#   [in]  "1"                     years in the curriculum, in which the course must be fit it
 #   [in]  "4"                     semesters per year
 #   [in]  "5"                     minimum credits per semeter
 #   [in]  "10"                    maximum credits per semeter
-racket ./src/scheduler.rkt tmp/output.json tmp/schedule.dot 2 4 5 10
+racket ./src/scheduler.rkt tmp/output.json tmp/schedule.dot 1 4 5 10
 
 # Visualize
 xdot tmp/schedule.dot &
